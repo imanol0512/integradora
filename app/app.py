8ee730d7f0415ed84cf67793cc50d9e99f109ee3
Imanol <taneima05@gmail.com>
2023-07-24 20:20:08 -0600

    05 proveedores version 1

--- app/app.py
+++ app/app.py
@@ -1,26 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+from models.proveedores import Proveedor
+
 app = Flask(__name__)
 
 # Simulación de datos de inicio de sesión (solo para propósito de demostración)
 USUARIO_VALIDO = {'email': 'usuario@example.com', 'password': 'secreto'}
-
-# Datos de ejemplo para el catálogo de productos
-productos = [
-    {
-        'nombre': 'Producto 1',
-        'precio': 10.99,
-        'stock': 5,
-        'imagen': 'producto1.jpg'
-    },
-    {
-        'nombre': 'Producto 2',
-        'precio': 15.99,
-        'stock': 10,
-        'imagen': 'producto2.jpg'
-    },
-    # Agregar más productos aquí si es necesario
-]
 
 @app.route('/')
 def index():
@@ -29,6 +14,22 @@ def index():
 
 @app.route('/catalogo_productos')
 def catalogo_productos():
+    # Datos de ejemplo para el catálogo de productos
+    productos = [
+        {
+            'nombre': 'Producto 1',
+            'precio': 10.99,
+            'stock': 5,
+            'imagen': 'producto1.jpg'
+        },
+        {
+            'nombre': 'Producto 2',
+            'precio': 15.99,
+            'stock': 10,
+            'imagen': 'producto2.jpg'
+        },
+        # Agregar más productos aquí si es necesario
+    ]
     # Aquí puedes realizar cualquier procesamiento necesario antes de mostrar la página
     return render_template('catalogoproducto.html', productos=productos)
 
@@ -47,5 +48,16 @@ def inicio_sesion():
 def sesion_admin():
     return render_template('indexadmin.html')
 
+@app.route('/proveedores')
+def lista_proveedores():
+    # Obtener la lista de proveedores desde la base de datos
+    proveedores = Proveedor.get_all()
+    return render_template('index.html', proveedores=proveedores)
+
+@app.route('/agregar_proveedor')
+def agregar_proveedor():
+    return render_template('agregar_proveedor.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
